37f25997a077308494ad884d9a450f55d3c722e1
Seray Uzgur <serayuzgur@gmail.com>
2018-01-21 16:41:02 +0300

    #20 AccessControlAllowOrigin added to all responses

--- src/service/utils.rs
+++ src/service/utils.rs
@@ -4,36 +4,48 @@
 use hyper::StatusCode;
 use hyper::server::Response;
 use hyper;
-use futures::{Future, BoxFuture};
+use hyper::header::AccessControlAllowOrigin;
+use futures::{BoxFuture, Future};
 use futures::future::ok;
 use hyper::header::ContentType;
 use serde_json;
 
 /// Prepares an error response , logs it, wraps to BoxFuture.
-pub fn error(response: Response,
-             code: StatusCode,
-             message: &str)
-             -> BoxFuture<Response, hyper::Error> {
-    ok(response.with_header(ContentType::plaintext())
+pub fn error(
+    response: Response,
+    code: StatusCode,
+    message: &str,
+) -> BoxFuture<Response, hyper::Error> {
+    ok(
+        response
+            .with_header(AccessControlAllowOrigin::Any)
+            .with_header(ContentType::plaintext())
             .with_status(code)
-            .with_body(message.to_string()))
-        .boxed()
+            .with_body(message.to_string()),
+    ).boxed()
 }
 
 /// Prepares an success response, wraps to BoxFuture.
-pub fn success(response: Response,
-               code: StatusCode,
-               value: &serde_json::Value)
-               -> BoxFuture<Response, hyper::Error> {
-    ok(response.with_header(ContentType::json())
+pub fn success(
+    response: Response,
+    code: StatusCode,
+    value: &serde_json::Value,
+) -> BoxFuture<Response, hyper::Error> {
+    ok(
+        response
+            .with_header(AccessControlAllowOrigin::Any)
+            .with_header(ContentType::json())
             .with_status(code)
-            .with_body(serde_json::to_vec(&value).unwrap()))
-        .boxed()
+            .with_body(serde_json::to_vec(&value).unwrap()),
+    ).boxed()
 }
 
 /// Splits '/'  and filters empty strings
 pub fn split_path(path: String) -> Vec<String> {
-    path.split("/").filter(|x| !x.is_empty()).map(String::from).collect::<Vec<String>>()
+    path.split("/")
+        .filter(|x| !x.is_empty())
+        .map(String::from)
+        .collect::<Vec<String>>()
 }
 
 #[cfg(test)]
@@ -43,15 +55,21 @@ mod tests {
     #[test]
     fn split_path_test() {
         // split_path("as/saß".to_string());
-        assert_eq!(split_path("".to_string()),
-                   Vec::<String>::new(),
-                   "Empty string must return empty vector.");
-        assert_eq!(split_path("/".to_string()),
-                   Vec::<String>::new(),
-                   "Empty string must return empty vector.");
-        assert_eq!(split_path("//".to_string()),
-                   Vec::<String>::new(),
-                   "Empty string must return empty vector.");
+        assert_eq!(
+            split_path("".to_string()),
+            Vec::<String>::new(),
+            "Empty string must return empty vector."
+        );
+        assert_eq!(
+            split_path("/".to_string()),
+            Vec::<String>::new(),
+            "Empty string must return empty vector."
+        );
+        assert_eq!(
+            split_path("//".to_string()),
+            Vec::<String>::new(),
+            "Empty string must return empty vector."
+        );
         assert_eq!(split_path("/posts".to_string()), vec!["posts"]);
         assert_eq!(split_path("/posts/".to_string()), vec!["posts"]);
         assert_eq!(split_path("/posts/1".to_string()), vec!["posts", "1"]);
